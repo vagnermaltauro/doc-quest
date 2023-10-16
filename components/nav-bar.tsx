10585a650eb74646e1e09af813f7fa83f5686593
vagnermaltauro <vagnermaltauro@gmail.com>
2023-10-16 16:59:39 -0300

    feat: install kinde and add buttons

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server';
+import { ArrowRight } from 'lucide-react';
 
 import { buttonVariants } from '@/components/ui/button';
 import MaxWidthWrapper from '@/components/max-width-wrapper';
@@ -22,6 +24,21 @@ export default function NavBar() {
               >
                 Pricing
               </Link>
+              <LoginLink
+                className={buttonVariants({
+                  variant: 'ghost',
+                  size: 'sm',
+                })}
+              >
+                Sign in
+              </LoginLink>
+              <RegisterLink
+                className={buttonVariants({
+                  size: 'sm',
+                })}
+              >
+                Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+              </RegisterLink>
             </>
           </div>
         </div>
