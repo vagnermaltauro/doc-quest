d16b77f8785ff7ad5c4130228e98976da15dabf3
vagnermaltauro <vagnermaltauro@gmail.com>
2023-10-18 20:43:41 -0300

    feat: initial page dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,22 @@
 import { redirect } from 'next/navigation';
+import { db } from '@/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
-export default function Page() {
+import { Dashboard } from '@/components/dashboard';
+
+export default async function Page() {
   const { getUser } = getKindeServerSession();
   const user = getUser();
 
   if (!user || !user.id) redirect('/auth-callback?origin=dashboard');
 
-  return <div>{user.email}</div>;
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) redirect('/auth-callback?origin=dashboard');
+
+  return <Dashboard />;
 }
