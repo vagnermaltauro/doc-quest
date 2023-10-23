595f6da4c0577842aafead87eab82eeca69adc3b
vagnermaltauro <vagnermaltauro@gmail.com>
2023-10-23 11:34:37 -0300

    feat: add date-fns library and custom dashboard

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,7 +1,10 @@
 'use client';
 
+import Link from 'next/link';
+import { format } from 'date-fns';
 import Skeleton from 'react-loading-skeleton';
 
+import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/icons';
 import { UploadButton } from '@/components/upload-button';
 import { trpc } from '@/app/_trpc/client';
@@ -16,7 +19,40 @@ export function Dashboard() {
       </div>
 
       {files && files?.length !== 0 ? (
-        <div>files</div>
+        <ul className="mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3">
+          {files
+            .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+            .map((file) => (
+              <li
+                key={file.id}
+                className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg"
+              >
+                <Link href={`/dashboard/${file.id}`} className="flex flex-col gap-2">
+                  <div className="pt-6 px-6 flex w-full items-center justify-between space-x-6">
+                    <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
+                    <div className="flex-1 truncate">
+                      <div className="flex items-center space-x-3">
+                        <h3 className="truncate text-lg font-medium text-zing-900">{file.name}</h3>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+                <div className="px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500">
+                  <div className="flex items-center gap-2">
+                    <Icons.plus className="h-4 w-4" />
+                    {format(new Date(file.createdAt), 'MMM yyyy')}
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <Icons.messageSquare className="h-4 w-4" />
+                    mocked
+                  </div>
+                  <Button size="sm" className="w-full" variant="destructive">
+                    <Icons.trash className="w-4 h-4" />
+                  </Button>
+                </div>
+              </li>
+            ))}
+        </ul>
       ) : isLoading ? (
         <Skeleton height={100} className="my-2" count={3} />
       ) : (
