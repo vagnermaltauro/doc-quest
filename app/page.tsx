f2ef5a5d61fa74510786cc2b1a1e9dff37d51689
vagnermaltauro <vagnermaltauro@gmail.com>
2023-10-25 17:31:23 -0300

    feat: update Doc Quest metadata and styling

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ import { MaxWidthWrapper } from '@/components/max-width-wrapper';
 
 export default function Home() {
   return (
-    <>
-      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+    <div className="bg-grid-slate-200/50">
+      <MaxWidthWrapper className="mb-12 pt-28 sm:pt-40 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
           <p className="text-sm font-semibold text-gray-700">Doc Quest is now public!</p>
         </div>
@@ -106,6 +106,6 @@ export default function Home() {
       <div className="bg-gradient-to-b from-transparent to-white pb-40 pt-24 text-center font-semibold text-slate-800">
         thanks for checking out Doc Quest
       </div>
-    </>
+    </div>
   );
 }
