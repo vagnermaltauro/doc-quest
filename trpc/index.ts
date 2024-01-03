95c7fc6441ce184588889a4c5fae86bc4c080c06
vagnermaltauro <vagnermaltauro@gmail.com>
2024-01-03 16:30:45 -0300

    feat: add getFileMessages procedure to retrieve file messages

--- trpc/index.ts
+++ trpc/index.ts
@@ -3,6 +3,9 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 
+import { INFINIT_QUERY_LIMIT } from '@/config/infinite-query';
+import { Messages } from '@/components/chat/messages';
+
 import { privateProcedure, publicProcedure, router } from './trpc';
 
 export const appRouter = router({
@@ -51,6 +54,55 @@ export const appRouter = router({
       if (!file) return { status: 'PENDING' as const };
 
       return { status: file.uploadStatus };
+    }),
+  getFileMessages: privateProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),
+        cursor: z.string().nullish(),
+        fileId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const { userId } = ctx;
+      const { fileId, cursor } = input;
+      const limit = input.limit ?? INFINIT_QUERY_LIMIT;
+      const file = await db.file.findFirst({
+        where: {
+          id: fileId,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: 'NOT_FOUND' });
+
+      const messages = await db.message.findMany({
+        take: limit + 1,
+        where: {
+          fileId,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+        cursor: cursor ? { id: cursor } : undefined,
+        select: {
+          id: true,
+          isUserMessage: true,
+          createdAt: true,
+          text: true,
+        },
+      });
+
+      let nextCursor: typeof cursor | undefined = undefined;
+      if (messages.length > limit) {
+        const nextItem = messages.pop();
+        nextCursor = nextItem?.id;
+      }
+
+      return {
+        Messages,
+        nextCursor,
+      };
     }),
   getFile: privateProcedure
     .input(z.object({ key: z.string() }))
