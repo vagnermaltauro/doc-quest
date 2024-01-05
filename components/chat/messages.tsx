6260c1f8f0e1f0aa045d708b5ad0342c251ef219
vagnermaltauro <vagnermaltauro@gmail.com>
2024-01-05 14:08:28 -0300

    fix: the UI layout issues and update dependencies

--- components/chat/messages.tsx
+++ components/chat/messages.tsx
@@ -34,7 +34,7 @@ export function Messages({ fileId }: MessagesProps) {
   const combinedMessages = [...(true ? [loadingMessage] : []), ...(messages ?? [])];
 
   return (
-    <div className="flex ma-h-[calc(100vh-3.5-7rem)] border-zing-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
+    <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-black scrollbar-thumb-rounded scrollbar-track-black-lighter scrollbar-w-2 scrolling-touch">
       {combinedMessages && combinedMessages.length > 0 ? (
         combinedMessages.map((message, index) => {
           const isNextMessageSamePerson =
