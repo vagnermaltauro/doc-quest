a671b2fe878aa391d41123eac14fb979acd4cb19
vagnermaltauro <vagnermaltauro@gmail.com>
2024-01-05 09:42:44 -0300

    refactor: refactor Messages component and update Prisma initialization

--- components/chat/messages.tsx
+++ components/chat/messages.tsx
@@ -1,3 +1,76 @@
-export function Messages() {
-  return <div>messages</div>;
+import Skeleton from 'react-loading-skeleton';
+
+import { INFINIT_QUERY_LIMIT } from '@/config/infinite-query';
+import { Message } from '@/components/chat/message';
+import { Icons } from '@/components/icons';
+import { trpc } from '@/app/_trpc/client';
+
+interface MessagesProps {
+  fileId: string;
+}
+
+export function Messages({ fileId }: MessagesProps) {
+  const { data, isLoading, fetchNextPage } = trpc.getFileMessages.useInfiniteQuery(
+    {
+      fileId,
+      limit: INFINIT_QUERY_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage?.nextCursor,
+      keepPreviousData: true,
+    },
+  );
+  const messages = data?.pages.flatMap((page) => page.messages);
+  const loadingMessage = {
+    createdAt: new Date().toISOString(),
+    id: 'loading-message',
+    isUserMessage: false,
+    text: (
+      <span className="flex h=full items-center justify-center">
+        <Icons.loader2 className="h-4 w-4 animate-spin" />
+      </span>
+    ),
+  };
+  const combinedMessages = [...(true ? [loadingMessage] : []), ...(messages ?? [])];
+
+  return (
+    <div className="flex ma-h-[calc(100vh-3.5-7rem)] border-zing-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
+      {combinedMessages && combinedMessages.length > 0 ? (
+        combinedMessages.map((message, index) => {
+          const isNextMessageSamePerson =
+            combinedMessages[index - 1]?.isUserMessage === combinedMessages[index]?.isUserMessage;
+
+          if (index === combinedMessages.length - 1) {
+            return (
+              <Message
+                key={message.id}
+                isNextMessageSamePerson={isNextMessageSamePerson}
+                message={message}
+              />
+            );
+          } else
+            return (
+              <Message
+                key={message.id}
+                isNextMessageSamePerson={isNextMessageSamePerson}
+                message={message}
+              />
+            );
+        })
+      ) : isLoading ? (
+        <div className="w-full flex flex-col gap-2">
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+        </div>
+      ) : (
+        <div className="flex-1 flex flex-col items-center justify-center gap-2">
+          <Icons.messageSquare className="h-8 w-8 text-blue-500" />
+          <h3 className="font-semibold text-xl">You&apos;re all set!</h3>
+          <p className="text-zinc-500 text-sm">Ask your first question to get started.</p>
+        </div>
+      )}
+    </div>
+  );
 }
--- db/index.ts
+++ db/index.ts
@@ -5,12 +5,14 @@ declare global {
 }
 
 let prisma: PrismaClient;
+
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
   if (!global.cachedPrisma) {
     global.cachedPrisma = new PrismaClient();
   }
+
   prisma = global.cachedPrisma;
 }
 
