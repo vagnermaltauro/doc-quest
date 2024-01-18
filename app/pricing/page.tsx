0166e088aab2af9758e2d5689985f986e0a45c48
vagnermaltauro <vagnermaltauro@gmail.com>
2024-01-18 19:21:48 -0300

    feat(pricing): add pricing page and create a content

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,3 +1,82 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+
+import { PLANS } from '@/config/stripe';
+import { cn } from '@/lib/utils';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { Icons } from '@/components/icons';
+import { MaxWidthWrapper } from '@/components/max-width-wrapper';
+
+import { pricingItems } from './_pricing-items';
+
 export default function PricingPage() {
-  return <div>Pricing Page</div>;
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
+  return (
+    <>
+      <MaxWidthWrapper className="mb-8 mt-24 text-center max-w-5xl">
+        <div className="mx-auto mb-10 sm:max-w-lg">
+          <h1 className="text-6xl font-bold sm:text-7xl">Pricing</h1>
+          <p className="pt-5 text-grey-600 sm:text-lg">
+            Wheter you&apos;re just trying out our service or need more, we&apos;ve got you covered.
+          </p>
+        </div>
+        <div className="pt-12 grid grid-cols-1 gap-10 lg:grid-cols-2">
+          <TooltipProvider>
+            {pricingItems.map(({ plan, tagline, quota, features }) => {
+              const price = PLANS.find((p) => p.slug === plan.toLowerCase())?.price.amount || 0;
+
+              return (
+                <div
+                  key={plan}
+                  className={cn('relative rounded-2xl bg-white shadow-lg', {
+                    'border-2 border-blue-600 shadow-blue-200': plan === 'Pro',
+                    'border border-gray-200': plan !== 'Pro',
+                  })}
+                >
+                  {plan === 'Pro' && (
+                    <div className="absolute -top-5 left-0 right-0 mx-auto w-32 rounded-full bg-gradient-to-r from-blue-600 to-cyan-600 px-3 py-2 text-sm font-medium text-white">
+                      Upgrade now
+                    </div>
+                  )}
+                  <div className="p-5">
+                    <h3 className="my-3 text-center font-display text-3xl font-bold">{plan}</h3>
+                    <p className="text-grey-500">{tagline}</p>
+                    <p className="my-5 font-display text-6xl font-semibold">${price}</p>
+                    <p className="text-grey-500">per month</p>
+                  </div>
+                  <div className="flex h-20 items-center justify-center border-b border-t border-gray-200 bg-gray-50">
+                    <div className="flex items-center space-x-1">
+                      <p>{quota.toLocaleString()} PDFs/mo included</p>
+                      <Tooltip delayDuration={300}>
+                        <TooltipTrigger className="cursor-default ml-1.5">
+                          <Icons.helpCircle className="h-4 w-4 text-zinc-500" />
+                        </TooltipTrigger>
+                        <TooltipContent className="w-80 p-2">
+                          How many PDFs you can upload per month
+                        </TooltipContent>
+                      </Tooltip>
+                    </div>
+                  </div>
+                  <ul className="my-10 space-y-5 px-8">
+                    {features.map(({ text, footnote, negative }) => {
+                      <li key={text} className="flex space-x-5">
+                        <div className="flex-shrink-0">
+                          {negative ? (
+                            <Icons.minus className="h6 w-6 text-gray-300" />
+                          ) : (
+                            <Icons.check className="h-6 w-6 text-green-500" />
+                          )}
+                        </div>
+                      </li>;
+                    })}
+                  </ul>
+                </div>
+              );
+            })}
+          </TooltipProvider>
+        </div>
+      </MaxWidthWrapper>
+    </>
+  );
 }
