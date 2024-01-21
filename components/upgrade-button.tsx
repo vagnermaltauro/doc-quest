b01002b01b91b0272d1133f9fd81d4c4f3db18f1
vagnermaltauro <vagnermaltauro@gmail.com>
2024-01-21 09:29:28 -0300

    feat: add stripe and pricing page

--- components/upgrade-button.tsx
+++ components/upgrade-button.tsx
@@ -1,9 +1,17 @@
+"use client"
+
 import { Button } from './ui/button';
 import { Icons } from './icons';
+import { trpc } from '@/app/_trpc/client';
 
 export function UpgradeButton() {
+  const { mutate: createStripeSession  } = trpc.createStripeSession.useMutation({
+    onSuccess: ({url}) => {
+      window.location.href = url ?? '/dashboard/billing'
+    }
+  })
   return (
-    <Button className="w-full">
+    <Button onClick={() => createStripeSession()} className="w-full">
       Upgrade now <Icons.arrowRight className="h-5 w-5 ml-1.5" />
     </Button>
   );
